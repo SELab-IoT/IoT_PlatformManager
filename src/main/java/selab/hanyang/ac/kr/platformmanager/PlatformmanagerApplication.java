56719aea27559e763bda18e08c38da3686bb0c4e
오용택 <ka123ak1@gmail.com>
2017-09-18 17:37:12 +0900

    [09/18] Platform Manager Initial commit
    1. Web framework로 Spring boot 사용 (Spring에서 xml로 설정하던 것들을 Java로 설정할 수 있도록 해주기 때문에 번거로운 설정을 별도로 익히지 않아도 됨)
    2. ORM으로 JPA 사용(예정) - 데이터베이스는 mysql 사용
    	2-1. ORM사용으로 데이터베이스구현과 관리가 쉬워질 것을 기대하며 개발 속도가 빨라질 것이라 생각됩니다. JPA는 Java 표준 ORM이기 때문에 사용하기에 적합하다고 생각됨

---
+++ src/main/java/selab/hanyang/ac/kr/platformmanager/PlatformmanagerApplication.java
@@ -0,0 +1,12 @@
+package selab.hanyang.ac.kr.platformmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlatformmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlatformmanagerApplication.class, args);
+	}
+}
