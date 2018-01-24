c651100494f6b66221646b0edaad019bec2c547b
frebern <frebern@naver.com>
2018-01-24 11:43:21 +0900

    [01/24]RequestParser 추가, PDPController 리팩토링

--- src/main/java/selab/hanyang/ac/kr/platformmanager/controller/DeviceRegister.java
+++ src/main/java/selab/hanyang/ac/kr/platformmanager/controller/DeviceRegister.java
@@ -1,4 +1,21 @@
 package selab.hanyang.ac.kr.platformmanager.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//윤근
+@Controller
 public class DeviceRegister {
+    @RequestMapping(value = "devices/{pepID}", method = RequestMethod.POST)
+    public @ResponseBody
+    String updateDeviceProfile(@PathVariable String pepID, HttpServletRequest request, HttpServletResponse httpResponse){
+
+        return null;
+    }
 }
